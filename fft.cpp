b76952497018aba7f22f82c22ad304b4788f05c5
Mateusz Cegiełka <mateusz@cegla.net>
2019-04-14 16:06:11 +0200

    added fft

---
+++ fft.cpp
@@ -0,0 +1,51 @@
+/// Name: FFT
+/// Description:
+/// Detail:
+/// Request: fft
+/// Guarantee: vector<C> fft(
+/// Dependencies:
+/// Parent:
+template <typename C, bool invert> vector<C> dft(vector<C> a) {
+    using R = typename C::value_type;
+    auto n = (int)a.size();
+    for (auto i=1, j=0; i<n; ++i) {
+        auto bit = n >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+        if (i < j)
+            swap(a[i], a[j]);
+    }
+    auto pi = acos((R)-1);
+    for (auto l=2; l<=n; l<<=1) {
+        auto angle = (invert ? -1 : 1) * 2 * pi / l;
+        auto wl = polar<R>(1, angle);
+        for (auto i=0; i<n; i+=l) {
+            auto w = (C)1;
+            for (auto j=0; j<l/2; ++j) {
+                auto y0 = a[i+j];
+                auto y1 = a[i+j+l/2] * w;
+                a[i+j] = y0 + y1;
+                a[i+j+l/2] = y0 - y1;
+                w *= wl;
+            }
+        }
+    }
+    if (invert)
+        for (auto& ai : a)
+            ai /= n;
+    return a;
+}
+template <typename C> vector<C> fft(vector<C> a, vector<C> b) {
+    auto n = 1;
+    while (n < (int)a.size() + (int)b.size() - 1)
+        n *= 2;
+    a.resize(n, (C)0);
+    b.resize(n, (C)0);
+    a = dft<C, false>(move(a));
+    b = dft<C, false>(move(b));
+    for (auto i=0; i<n; ++i)
+        a[i] *= b[i];
+    a = dft<C, true>(move(a));
+    return a;
+}
