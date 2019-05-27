78b311feb87d11957b399ac15b91a03db296caff
Mateusz Cegiełka <mateusz@cegla.net>
2019-05-27 10:51:00 +0200

    change name of .empty to .id in segtree

--- segtree.cpp
+++ segtree.cpp
@@ -8,7 +8,7 @@
 template <typename T> struct Segtree {
     Segtree(int n):base(1){
         while (base < n) base *= 2;
-        nodes.resize(base*2, T::empty());
+        nodes.resize(base*2, T::id());
     }
     void setLeaf(int i, T x) {
         nodes[base+i] = x;
@@ -17,7 +17,7 @@ template <typename T> struct Segtree {
     }
     T readRange(int left, int right) const { return implRR(left, right, 0, base, 1); }
     T implRR(int left, int right, int i, int e, int v) const {
-        if (left >= e or right <= i) return T::empty();
+        if (left >= e or right <= i) return T::id();
         if (left <= i and right >= e) return nodes[v];
         return T::merge(implRR(left, right, i, (i+e)/2, v*2), implRR(left, right, (i+e)/2, e, v*2+1));
     }
