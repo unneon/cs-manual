7ac4edb8ef39f128dbbdd903f634ebaeecfc66f4
Mateusz Cegiełka <mateusz@cegla.net>
2019-08-14 16:47:06 +0200

    add scaler

---
+++ scaler.cpp
@@ -0,0 +1,22 @@
+/// Name: Scaler
+/// Description:
+/// Detail:
+/// Request: Scaler
+/// Guarantee: struct Scaler {
+/// Dependencies:
+/// Parent:
+template <typename T> struct Scaler {
+	struct Build {
+		vector<T> xs;
+		void notice(T x) { xs.push_back(x); }
+		Scaler<T> prepare() {
+			sort(xs.begin(), xs.end());
+			xs.erase(unique(xs.begin(), xs.end()), xs.end());
+			return Scaler<T>{move(xs)};
+		}
+	};
+	int scale(const T& x) { return (int)(lower_bound(xs.begin(), xs.end(), x) - xs.begin()); }
+	const T& unscale(int i) { return xs[i]; }
+	int size() const { return (int)xs.size(); }
+	vector<T> xs;
+};
